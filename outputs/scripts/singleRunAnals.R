61fa0867c31eb750337b79060e05574831557f98
bulentozel <bulent.ozel@gmail.com>
2014-02-21 15:08:20 +0100

    Analyses on closed economy.

--- outputs/scripts/singleRunAnals.R
+++ outputs/scripts/singleRunAnals.R
@@ -1,45 +1,122 @@
 ### Init Workspace  ###
 rm(list = ls())
-source("/Users/bulent/Documents/AWorkspace/iceace/FLAME/outputs/scripts/LibIceace.R", echo = TRUE)
-data_dir = "/Users/bulent/Documents/AWorkspace/iceace/FLAME/outputs/data"
-output_dir = "/Users/bulent/Documents/AWorkspace/iceace/FLAME/outputs/plots/"
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/LibIceace.R", echo = TRUE)
+data_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/data"
+output_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/plots/"
 setwd(output_dir)
 
 # Import Datasets #
-data_household = 1
-source("/Users/bulent/Documents/AWorkspace/iceace/FLAME/outputs/scripts/importSimResults.R", echo = T)
+data_household = 0
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/importSimResults.R", echo = T)
 
 # Plotting Mall, Real Estate, Government, Central Bank and Equity Fund Vars #
-source("/Users/bulent/Documents/AWorkspace/iceace/FLAME/outputs/scripts/plotAgentVars.R", echo = T)
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/plotAgentVars.R", echo = T)
 
 
 # Plot Bank Vars #
-bankA = 156
-bankB = 157
-source("/Users/bulent/Documents/AWorkspace/iceace/FLAME/outputs/scripts/plotBanksVars.R", echo = T)
+bankA = 1637
+bankB = 1638
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/plotBanksVars.R", echo = T)
 
 # Plot Firm Vars #
-source("/Users/bulent/Documents/AWorkspace/iceace/FLAME/outputs/scripts/plotFirmsVars.R", echo = T)
+source("/Users/bulent/Documents/AWorkspace/iceace/rm(list = ls())
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/LibIceace.R", echo = TRUE)
+data_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/data"
+output_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/plots/"
+setwd(output_dir)
+
+# Import Datasets #
+data_household = 0
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/importSimResults.R", echo = T)
+
+# Plotting Mall, Real Estate, Government, Central Bank and Equity Fund Vars #
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/plotAgentVars.R", echo = T)/outputs/scripts/plotFirmsVars.R", echo = T)
 
 
 # Compute GDP #
 betaval = "0.30"
-source("/Users/bulent/Documents/AWorkspace/iceace/FLAME/outputs/scripts/computeGDPs.R", echo = T)
+source("/Users/bulent/Documents/AWorkspace/iceace/rm(list = ls())
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/LibIceace.R", echo = TRUE)
+data_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/data"
+output_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/plots/"
+setwd(output_dir)
+
+# Import Datasets #
+data_household = 0
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/importSimResults.R", echo = T)
+
+# Plotting Mall, Real Estate, Government, Central Bank and Equity Fund Vars #
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/plotAgentVars.R", echo = T)/outputs/scripts/computeGDPs.R", echo = T)
 
 # Compute Writeoff Cases #
 niter = 3600
-source("/Users/bulent/Documents/AWorkspace/iceace/FLAME/outputs/scripts/computeWriteoffs.R", echo = T)
+source("/Users/bulent/Documents/AWorkspace/iceace/rm(list = ls())
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/LibIceace.R", echo = TRUE)
+data_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/data"
+output_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/plots/"
+setwd(output_dir)
+
+# Import Datasets #
+data_household = 0
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/importSimResults.R", echo = T)
+
+# Plotting Mall, Real Estate, Government, Central Bank and Equity Fund Vars #
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/plotAgentVars.R", echo = T)/outputs/scripts/computeWriteoffs.R", echo = T)
 
 # Compute Crediting Frequencies #
-source("/Users/bulent/Documents/AWorkspace/iceace/FLAME/outputs/scripts/computeCreditings.R", echo = T)
+source("/Users/bulent/Documents/AWorkspace/iceace/rm(list = ls())
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/LibIceace.R", echo = TRUE)
+data_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/data"
+output_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/plots/"
+setwd(output_dir)
+
+# Import Datasets #
+data_household = 0
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/importSimResults.R", echo = T)
+
+# Plotting Mall, Real Estate, Government, Central Bank and Equity Fund Vars #
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/plotAgentVars.R", echo = T)/outputs/scripts/computeCreditings.R", echo = T)
 
 # Plot Household Vars #
 if (data_household){
-	source("/Users/bulent/Documents/AWorkspace/iceace/FLAME/outputs/scripts/plotHouseholdVars.R", echo = T)
+	source("/Users/bulent/Documents/AWorkspace/iceace/rm(list = ls())
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/LibIceace.R", echo = TRUE)
+data_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/data"
+output_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/plots/"
+setwd(output_dir)
+
+# Import Datasets #
+data_household = 0
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/importSimResults.R", echo = T)
+
+# Plotting Mall, Real Estate, Government, Central Bank and Equity Fund Vars #
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/plotAgentVars.R", echo = T)/outputs/scripts/plotHouseholdVars.R", echo = T)
 	}
 
 # Compute Housing Market Activities#
-source("/Users/bulent/Documents/AWorkspace/iceace/FLAME/outputs/scripts/computeHousing.R", echo = T)
+source("/Users/bulent/Documents/AWorkspace/iceace/rm(list = ls())
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/LibIceace.R", echo = TRUE)
+data_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/data"
+output_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/plots/"
+setwd(output_dir)
 
-source("/Users/bulent/Documents/AWorkspace/iceace/FLAME/outputs/scripts/identityCheck.R", echo = T)
+# Import Datasets #
+data_household = 0
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/importSimResults.R", echo = T)
+
+# Plotting Mall, Real Estate, Government, Central Bank and Equity Fund Vars #
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/plotAgentVars.R", echo = T)/outputs/scripts/computeHousing.R", echo = T)
+
+source("/Users/bulent/Documents/AWorkspace/iceace/rm(list = ls())
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/LibIceace.R", echo = TRUE)
+data_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/data"
+output_dir = "/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/plots/"
+setwd(output_dir)
+
+# Import Datasets #
+data_household = 0
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/importSimResults.R", echo = T)
+
+# Plotting Mall, Real Estate, Government, Central Bank and Equity Fund Vars #
+source("/Users/bulent/Documents/AWorkspace/iceace/IceaceModel1.0/outputs/scripts/plotAgentVars.R", echo = T)/outputs/scripts/identityCheck.R", echo = T)
 
